919975c97f207e6c639aee19136d84165c590b60
Mauro Vanetti <mauro_vanetti@docenti.naba.it>
2025-12-05 13:28:29 +0100

    Turbo boost

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -7,11 +7,14 @@ public class PlayerControllerX : MonoBehaviour
 {
     private Rigidbody playerRb;
     private float speed = 500;
+    private float turboSpeed = 1500;
     private GameObject focalPoint;
 
     public bool hasPowerup;
     public GameObject powerupIndicator;
     public int powerUpDuration = 5;
+
+    public GameObject smoke;
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
@@ -20,14 +23,33 @@ public class PlayerControllerX : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+        smoke = GameObject.Find("Smoke_Particle");
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {            
+            smoke.GetComponent<ParticleSystem>().Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+             smoke.GetComponent<ParticleSystem>().Stop();
+        }
+
         // Add force to player in direction of the focal point (and camera)
         float verticalInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
-
+        if (Input.GetKey(KeyCode.Space))
+        {
+            smoke.transform.position = transform.position;
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * turboSpeed * Time.deltaTime); 
+        }
+        else
+        {
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+        }
+            
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
 
